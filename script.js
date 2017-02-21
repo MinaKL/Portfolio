561a94eacede881b0941a77fc5ed92fa9eb72d9f
Mina <minakatie@comcast.net>
2017-02-21 00:48:38 +0000

    I changed stuff 2/20/17

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
     //Write all your jQuery code here
-        $("button").click(function() {
+        $("#button1").click(function() {
       $("#like").html("You like this project!");
     });
     
+    $("#button2").click(function() {
+      $("#like2").html("You like this project!");
+    });
     
     
     
