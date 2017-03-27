795e75895712bc4484f773a360e074a5e75261f7
Mina <minakatie@comcast.net>
2017-03-27 20:40:19 +0000

    3/27 reloading change

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
     //Write all your jQuery code here
+    int count = 0;
         $("#button1").click(function() {
-      $("#like").html("You like this project!");
+            count++;
+      $("#like").html("You like this project!" + count);
+      
     });
     
     $("#button2").click(function() {
       $("#like2").html("You like this project!");
     });
     
+    function modify_qty(val) {
+    var qty = document.getElementById('qty').value;
+    var new_qty = parseInt(qty,10) + val;
     
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+    
+    document.getElementById('qty').value = new_qty;
+    return new_qty;
+}
     
 });
